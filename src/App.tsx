bfa333f8d9c366c7e3617ec4ece244b03f08ab54
rasul-surname <rasul.surname@mail.ru>
2025-03-27 17:48:07 +0300

    Правки

--- src/App.tsx
+++ src/App.tsx
@@ -2,136 +2,149 @@ import { useEffect, useState } from 'react'
 import './MemoGame.css'
 
 type Card = {
-	id: number;
-	emoji: string;
-	flipped: boolean;
-	matched: boolean;
-};
+	id: number
+	emoji: string
+	flipped: boolean
+	matched: boolean
+}
+
+const levels = [
+	{ rows: 2, cols: 3 },
+	{ rows: 3, cols: 4 },
+	{ rows: 4, cols: 4 }
+]
 
 const cardsArray: string[] = ['🚇', '🚉', '🚦', '🚧', '🚏', '🚥', '🚊', '⚠️']
-const shuffledCards: string[] = [...cardsArray, ...cardsArray].sort(() => Math.random() - 0.5)
+const generateShuffledCards = (rows: number, cols: number): Card[] => {
+	const totalPairs = (rows * cols) / 2
+	const selectedEmojis = cardsArray.slice(0, totalPairs)
+
+	return [...selectedEmojis, ...selectedEmojis]
+		.sort(() => Math.random() - 0.5)
+		.map((emoji, index) => ({ id: index, emoji, flipped: false, matched: false }))
+}
 
 function App() {
-	const [cards, setCards] = useState<Card[]>(shuffledCards.map((emoji, index) => ({
-		id: index,
-		emoji,
-		flipped: true,
-		matched: false
-	})))
-
-	const [lives, setLives] = useState(30)
-	const [isGameWon, setIsGameWon] = useState(false)
-	const [isGameLost, setIsGameLost] = useState(false)
-	const [countdown, setCountdown] = useState(5)
-
-	useEffect(() => {
-		if (countdown > 0) {
-			const timer = setInterval(() => {
-				setCountdown(prev => prev - 1)
-			}, 1000)
-			return () => clearInterval(timer)
-		} else {
-			setCards(prevCards => prevCards.map(card => ({ ...card, flipped: false })))
-		}
-	}, [countdown])
-
-	useEffect(() => {
-		if (cards.every(card => card.matched)) {
-			setIsGameWon(true)
-		}
-
-		// Проверка на поражение
-		if (lives === 0) {
-			setIsGameLost(true)
-		}
-	}, [cards, lives])
-
+	const [cards, setCards] = useState<Card[]>([])
 	const [flippedCards, setFlippedCards] = useState<number[]>([])
+	const [isGameStarted, setIsGameStarted] = useState<boolean>(false)
+	const [score, setScore] = useState<number>(0)
+	const [timer, setTimer] = useState<number>(20)
+	const [levelIndex, setLevelIndex] = useState<number>(0)
 
-	const handleCardClick = (id: number) => {
-		if (lives === 0 || flippedCards.length === 2 || cards.find(card => card.id === id)?.flipped) return
+	useEffect(() => {
+		if (isGameStarted) {
+			const { rows, cols } = levels[levelIndex]
+			setCards(generateShuffledCards(rows, cols))
+		}
+	}, [isGameStarted, levelIndex])
 
-		const newCards = cards.map(card =>
-			card.id === id ? { ...card, flipped: true } : card
-		)
+	useEffect(() => {
+		if (isGameStarted && timer > 0) {
+			const timer = setInterval(() => {
+				setTimer((prev: number) => prev - 1)
+			}, 1000)
 
+			return () => clearInterval(timer)
+		} else if (timer === 0) {
+			console.log('отправлен запрос')
+			sendScore()
+			setIsGameStarted(false)
+		}
+	}, [isGameStarted, timer])
+
+	useEffect(() => {
+		if (cards.length > 0 && cards.every(card => card.matched)) {
+			if (levelIndex < levels.length - 1) {
+				setLevelIndex(prev => prev + 1)
+				setIsGameStarted(true)
+				setTimer(20)
+			} else {
+				console.log('последний уровень пройден')
+				setIsGameStarted(false)
+				// sendScore()
+			}
+		}
+	}, [cards])
+
+	const handleCardClick = (id: number): void => {
+		if (!isGameStarted) return
+
+		if (flippedCards.length === 2 || cards.find(card => card.id === id)?.flipped) return
+
+
+		const newCards = cards.map(card => card.id === id ? { ...card, flipped: true } : card)
 		setCards(newCards)
 		setFlippedCards([...flippedCards, id])
 
 		if (flippedCards.length === 1) {
 			const firstCard = cards.find(card => card.id === flippedCards[0])
 			const secondCard = newCards.find(card => card.id === id)
+
 			if (firstCard && secondCard && firstCard.emoji === secondCard.emoji) {
 				setTimeout(() => {
 					setCards(prevCards => prevCards.map(card =>
 						card.emoji === firstCard.emoji ? { ...card, matched: true } : card
 					))
+					setScore((prevScore) => prevScore + 10)
+					setFlippedCards([])
 				}, 500)
 			} else {
-				setLives(prevLives => {
-					if (prevLives === 1) {
-						return 0
-					}
-					return prevLives - 1
-				})
 				setTimeout(() => {
 					setCards(prevCards => prevCards.map(card =>
 						flippedCards.includes(card.id) || card.id === id ? { ...card, flipped: false } : card
 					))
+
+					setFlippedCards([])
 				}, 1000)
 			}
-			setFlippedCards([])
 		}
 	}
 
-	const handleRestart = () => {
-		setCards(shuffledCards.map((emoji, index) => ({
-			id: index,
-			emoji,
-			flipped: true,
-			matched: false
-		})))
-		setLives(3)
-		setIsGameWon(false)
-		setIsGameLost(false)
-		setCountdown(5)
+	const sendScore = (): void => {
+		fetch('/api/score', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ score })
+		}).catch(err => console.error('Ошибка отправки счёта:', err))
+	}
+
+	const handleGameStart = () => {
+		setIsGameStarted(true)
+		setTimer(20)
+		setScore(0)
+		setLevelIndex(0)
 	}
 
 	return (
 		<div className="wrapper">
-			<div className="lives">❤️ {lives}</div>
-			{countdown > 0 ? (
-				<div className="countdown">⏳ До начала игры: {countdown}</div>
-			) : null}
-			{!isGameWon && !isGameLost ? (
-				<div className="memo-game">
-					{cards.map(card => (
-						<div
-							key={card.id}
-							className={`card ${card.flipped || card.matched ? 'flipped' : ''}`}
-							onClick={() => handleCardClick(card.id)}
-						>
-							<div className="card-inner">
-								<div className="card-front"></div>
-								<div className="card-back">{card.emoji}</div>
+			{!isGameStarted ? (
+				<button className="button-restart" onClick={handleGameStart}>
+					НАЧАТЬ ИГРУ
+				</button>
+			) : (
+				<>
+					<div className="game-info">
+						<div className="level">Уровень: {levelIndex + 1} / {levels.length}</div>
+						<div className="timer">⏳ {timer}</div>
+						<div className="score">Очки: {score}</div>
+					</div>
+					<div className="memo-game"
+						 style={{ gridTemplateColumns: `repeat(${levels[levelIndex].cols}, 80px)` }}>
+						{cards.map(card => (
+							<div
+								key={card.id}
+								className={`card ${card.flipped || card.matched ? 'flipped' : ''}`}
+								onClick={() => handleCardClick(card.id)}
+							>
+								<div className="card-inner">
+									<div className="card-front"></div>
+									<div className="card-back">{card.emoji}</div>
+								</div>
 							</div>
-						</div>
-					))}
-				</div>
-			) : null}
-
-			{isGameWon && (
-				<div className="banner">
-					<h2>Поздравляем, вы выиграли! 🎉</h2>
-					<button className="button-restart" onClick={handleRestart}>Начать заново</button>
-				</div>
-			)}
-
-			{isGameLost && (
-				<div className="banner">
-					<h2>Игра окончена! Вы проиграли. 😢</h2>
-					<button className="button-restart" onClick={handleRestart}>Начать заново</button>
-				</div>
+						))}
+					</div>
+				</>
 			)}
 		</div>
 	)
