84cf53853c8ced7cd1540c1bb711485c3c9198c6
rasul-surname <rasul.surname@mail.ru>
2025-04-09 16:05:00 +0300

    Поправлены ошибки при сборке

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
 // https://vitejs.dev/config/
 
-export default ({ mode }) => {
+export default ({ mode }: { mode: string }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
 	return defineConfig({
