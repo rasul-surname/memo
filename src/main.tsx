8db2024fa31f9a628f1760b7b6347c5f8da286c7
rasul-surname <rasul.surname@mail.ru>
2025-04-10 11:40:19 +0300

    Правки для сборки

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from '@/src/components/ui/provider.tsx'
+import { Provider } from './components/ui/provider.tsx'
 import App from '@/src/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
